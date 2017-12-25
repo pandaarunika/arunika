f59850aaefa3ebdb07253692e7f4c298fea1c9da
Arya <arya.mishra@indusface.com>
2017-12-25 13:59:19 +0530

    thread sync

---
+++ thread3/src/synchronisedNow/App.java
@@ -0,0 +1,46 @@
+package synchronisedNow;
+
+public class App {
+
+	public static void main(String[] args) {
+		A a= new A();
+		B b=new B(a);
+		C c = new C(a);
+		b.start();
+		c.start();
+	}
+
+}
+class A {
+	synchronized void print(int n){//synchronized method
+		for(int i =1;i<=5;i++){
+			System.out.println(i*n);
+		}
+	}
+}
+class B extends Thread{
+	A a;
+	public B(A a){
+		this.a=a;
+	}
+	public void run(){
+		a.print(10);
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class C extends Thread{
+	A a;
+	public C(A a){
+		this.a=a;
+	}
+	public void run(){
+		a.print(100);
+	}
+}
+
